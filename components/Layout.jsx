7a2efd9e558cdc0e26be922d2ac02199a5475e2a
Mikhail <mishanyaveselyi@gmail.com>
2022-05-05 23:04:01 -0400

    Set layout

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Head from 'next/head'
+import NavBar from './NavBar'
 
 const Layout = () => {
   return (
-    <div>Layout</div>
+    <div>
+      <div className='layout'>
+        <Head>
+          <title>CarCare Store</title>
+        </Head>
+        <header>
+          <NavBar />
+        </header>
+        <main className='main-container'>
+          EMPTY
+        </main>
+        <footer>
+          <Footer />
+        </footer>
+      </div>
+    </div>
   )
 }
 
