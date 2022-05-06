4cde57f9302652b0c32575902cdbf3cbe6af2d29
Mikhail <mishanyaveselyi@gmail.com>
2022-05-05 23:20:09 -0400

    Set Navbar

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import Head from 'next/head'
 import NavBar from './NavBar'
+import Footer from './Footer'
 
-const Layout = () => {
+const Layout = ({children}) => {
   return (
     <div>
       <div className='layout'>
@@ -13,7 +14,7 @@ const Layout = () => {
           <NavBar />
         </header>
         <main className='main-container'>
-          EMPTY
+          {children}
         </main>
         <footer>
           <Footer />
