d90f0effb605653e5e4aaddd35db27d85629ef39
Mikhail <mishanyaveselyi@gmail.com>
2022-05-09 14:51:31 -0400

    set increase qty function

--- context/StateContext.js
+++ context/StateContext.js
@@ -4,7 +4,26 @@ import { toast }from 'react-hot-toast'
 const Context = createContext()
 
 export const StateContext = ({ children }) => {
+  const [showCart, setShowCart] = useState(false)
+  const [cartItems, setCartItems] = useState()
+  const [totalPrice, setTotalPrice] = useState()
+  const [totalQuantities, setTotalQuantities] = useState()
+  const [qty, setQty] = useState(1)
 
-  
+  const incQty = () => {
+    setQty((prevQty) => prevQty + 1)
+  }
 
+  return (
+    <Context.Provider
+    value={{
+      showCart,
+      cartItems,
+      totalPrice,
+      totalQuantities,
+      qty
+    }}>
+      { children }
+    </Context.Provider>
+  )
 }
