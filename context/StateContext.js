12264698fc7bb4c77acd65e9140c5bc4c5d8b3e3
Mikhail <mishanyaveselyi@gmail.com>
2022-05-09 14:55:48 -0400

    Set decrease qty function

--- context/StateContext.js
+++ context/StateContext.js
@@ -14,6 +14,13 @@ export const StateContext = ({ children }) => {
     setQty((prevQty) => prevQty + 1)
   }
 
+  const decQty = () => {
+    setQty((prevQty) => {
+      if(prevQty - 1 < 1) return 1;
+      return prevQty - 1
+    })
+  }
+
   return (
     <Context.Provider
     value={{
