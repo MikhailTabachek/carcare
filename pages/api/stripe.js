83138e91a46cc08e4c574f8d4e7d4bc7d9aeacea
Mikhail <mishanyaveselyi@gmail.com>
2022-05-11 19:30:32 -0400

    Spelling errors fix

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,4 +1,6 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+import Stripe from 'stripe';
+
+const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
@@ -10,11 +12,11 @@ export default async function handler(req, res) {
         mode: 'payment',
         payment_method_types: ['card'],
         billing_address_collection: 'auto',
-        shipping_oprions: [
+        shipping_options: [
           {shipping_rate: 'shr_1KyN7IBdcf1QltKsz7Hy5Yb2'},
           {shipping_rate: 'shr_1KyN8IBdcf1QltKskaIBz8YV'}
         ],
-        line_items: req.body.cartItems.map((item) => {
+        line_items: req.body.map((item) => {
           const img = item.image[0].asset._ref
           const newImage = img.replace('image-', 'https://cdn.sanity.io/images/plp5soxd/production/').replace('-webp', '.webp')
 
@@ -29,7 +31,7 @@ export default async function handler(req, res) {
             },
             adjustable_quantity: {
               enabled:true,
-              minimum:1,
+              minimum: 1,
             },
             quantity: item.quantity
           }
