5042c8c31a7d876ca4c00b626f737bc26406ecd8
Mikhail <mishanyaveselyi@gmail.com>
2022-05-11 19:00:29 -0400

    Set stripe checkout session

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -2,6 +2,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
+    console.log(req.body.cartItem)
     try {
 
       const params = {
@@ -13,25 +14,38 @@ export default async function handler(req, res) {
           {shipping_rate: 'shr_1KyN7IBdcf1QltKsz7Hy5Yb2'},
           {shipping_rate: 'shr_1KyN8IBdcf1QltKskaIBz8YV'}
         ],
-        line_items: [
-          {
-            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-            price: '{{PRICE_ID}}',
-            quantity: 1,
-          },
-        ],
-        mode: 'payment',
+        line_items: req.body.cartItems.map((item) => {
+          const img = item.image[0].asset._ref
+          const newImage = img.replace('image-', 'https://cdn.sanity.io/images/plp5soxd/production/').replace('-webp', '.webp')
+
+          return {
+            price_data: {
+              currency: 'usd',
+              product_data: {
+                name: item.name,
+                images: [newImage],
+              },
+              unit_amount: item.price * 100,
+            },
+            adjustable_quantity: {
+              enabled:true,
+              minimum:1,
+            },
+            quantity: item.quantity
+          }
+        }),
         success_url: `${req.headers.origin}/?success=true`,
         cancel_url: `${req.headers.origin}/?canceled=true`,
       }
       // Create Checkout Sessions from body params.
-      const session = await stripe.checkout.sessions.create(params);
-      res.redirect(303, session.url);
+      const session = await stripe.checkout.sessions.create(params)
+      res.status(200).json(session)
+      res.redirect(303, session.url)
     } catch (err) {
-      res.status(err.statusCode || 500).json(err.message);
+      res.status(err.statusCode || 500).json(err.message)
     }
   } else {
-    res.setHeader('Allow', 'POST');
-    res.status(405).end('Method Not Allowed');
+    res.setHeader('Allow', 'POST')
+    res.status(405).end('Method Not Allowed')
   }
 }
